dc3466b79a0e7384d46bc91dcec47f7225aa19a9
Wali Morris <walimmorris@gmail.com>
2022-11-17 21:30:59 -0800

    add getVirtualHostInstanceId() that returns the instance id of ubuntu virtual host

--- src/main/java/io/com/morris/emerald01/controller/TestController.java
+++ src/main/java/io/com/morris/emerald01/controller/TestController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 @RestController
@@ -44,5 +47,30 @@ public class TestController {
             }
         });
         return new Position(positionsList);
+    }
+
+    @GetMapping("/instance")
+    public ResponseEntity<?> getVirtualHostInstanceId() throws IOException {
+        Runtime runtime = Runtime.getRuntime();
+        String[] commands = {"ec2-metadata", "-i"};
+        Process proc = runtime.exec(commands);
+
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+
+        StringBuilder result = new StringBuilder();
+        String s;
+
+        while ((s = stdInput.readLine()) != null) {
+            result.append(s);
+        }
+
+        while ((s = stdError.readLine()) != null) {
+            result.append(s);
+        }
+        stdInput.close();
+        stdError.close();
+
+        return ResponseEntity.ok(result.toString());
     }
 }
